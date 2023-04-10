45201bba03ed9de735aa1d7f93388f5278c53d33
Sebastian Cabrera <sebacabrera.96@hotmail.com>
2023-04-10 20:23:05 -0300

    name corrections

--- project/program.py
+++ project/program.py
@@ -122,42 +122,41 @@ def invoice_ticket_load():
     "Total Invoice/Ticket Amount"
     return
 
-def operate_on_database():
-    option = input("What action would you like to perform on? 1- Load Bills/Invoices/Other Docs. \n 2- other option\n Answer: ")
+def operate_on_database(database):
+    option = input(f"What action would you like to perform on database {database}?\n 1- Load Bills/Invoices/Other Docs.\n 2- (Incoming) Other option\n Answer: ")
     while option not in ["1", "2", "3", "4"]:
         option = input("What would you like to do? 1- Load Bills/Invoices/Other Docs. \n 2- other option")
     if option == "1":
         invoice_ticket_load()
-
     return
 
-operate_on_database()
+# operate_on_database("coca")
 
-# option = input("Hello, Welcome to this Accounting Software! Please enter the number of the action you would like to perform: \n 1- Create New Database. \n 2- Delete an existing Database. \n 3- Operate with an existing Database.\n 4- Close the Program.\n Answer: ")
+option = input("Hello, Welcome to this Accounting Software! Please enter the number of the action you would like to perform: \n 1- Create New Database. \n 2- Delete an existing Database. \n 3- Operate with an existing Database.\n 4- Close the Program.\n Answer: ")
 
-# while option not in ["1", "2", "3", "4"]:
-#         option = input("Please enter the number of the action you would like to do: \n 1- Create New Database. \n 2- Delete an existing Database. \n 3- Operate with an existing Database.\n 4- Close the Program.\n Answer: ")
-# if option == "1":
-#     sv_data_and_db_name = import_or_manual_sv_data_gathering()
-#     if sv_data_and_db_name[0] == "with env":
-#         env_data = read_env_file(sv_data_and_db_name[1])
-#         create_database(env_data[0], env_data[1], env_data[2], f"{sv_data_and_db_name[2]}")
-#         connect_and_execute_query(env_data[0], env_data[1], env_data[2], f"{sv_data_and_db_name[2]}", "create_tables")
-#     else:
-#         data_list = []
-#         for data in sv_data_and_db_name[1]:
-#             data_list.append(data)
-#         create_database(data_list[0], data_list[1], data_list[2], f"{sv_data_and_db_name[2]}")
-#         connect_and_execute_query(data_list[0], data_list[1], data_list[2], f"{sv_data_and_db_name[2]}", "create_tables")
-# elif option == "2":
-#         try:
-#             data = ask_for_sv_data()
-#             database_name = input("Please insert Database name: ")
-#             delete_database(data[0], data[1], data[2], database_name)
-#         except DatabaseError as er:
-#             print(er)
-# elif option == "4":
-#     print("Closing program! Have a nice day.")
+while option not in ["1", "2", "3", "4"]:
+        option = input("Please enter the number of the action you would like to do: \n 1- Create New Database. \n 2- Delete an existing Database. \n 3- Operate with an existing Database.\n 4- Close the Program.\n Answer: ")
+if option == "1":
+    sv_data_and_db_name = import_or_manual_sv_data_gathering()
+    if sv_data_and_db_name[0] == "with env":
+        env_data = read_env_file(sv_data_and_db_name[1])
+        create_database(env_data[0], env_data[1], env_data[2], f"{sv_data_and_db_name[2]}")
+        connect_and_execute_query(env_data[0], env_data[1], env_data[2], f"{sv_data_and_db_name[2]}", "create_tables")
+    else:
+        data_list = []
+        for data in sv_data_and_db_name[1]:
+            data_list.append(data)
+        create_database(data_list[0], data_list[1], data_list[2], f"{sv_data_and_db_name[2]}")
+        connect_and_execute_query(data_list[0], data_list[1], data_list[2], f"{sv_data_and_db_name[2]}", "create_tables")
+elif option == "2":
+        try:
+            data = ask_for_sv_data()
+            database_name = input("Please insert Database name: ")
+            delete_database(data[0], data[1], data[2], database_name)
+        except DatabaseError as er:
+            print(er)
+elif option == "4":
+    print("Closing program! Have a nice day.")
 elif option == "3":
     sv_data_and_db_name=import_or_manual_sv_data_gathering()
     if sv_data_and_db_name[0] == "with env":
@@ -171,4 +170,4 @@ elif option == "3":
     if len(checking_sv_conn)==0:
         print("Connection Error!")
     else:
-        operate_on_database()
+        operate_on_database(sv_data_and_db_name[2])
